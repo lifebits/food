d364d636525842f5898017b35261250a95e19afe
Nikolay Popov <mail@lifebits.ru>
2017-04-09 11:18:01 +0700

    added main title block

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public searchMode: 'ingredients' | 'recipe' = 'ingredients';
+
+  public searchPlaceholder: string = this.getSearchPlaceholder();
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  toggleSearchMode(searchMode): void {
+    this.searchMode = searchMode;
+    this.searchPlaceholder = this.getSearchPlaceholder();
+    console.log(searchMode);
+  }
+
+  getSearchPlaceholder(): string {
+    let placeholderList = {
+      ingredients: 'Искать ингредиенты для блюд',
+      recipe: 'Искать блюда'
+    };
+    return placeholderList[this.searchMode];
   }
 
 }
